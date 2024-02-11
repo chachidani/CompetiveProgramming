5850e6c873c4cab93596797c6874dbf7405d4575
chachidani <141312783+chachidani@users.noreply.github.com>
2024-02-11 10:30:19 +0300

    Add solution for Majority Element

---
+++ leetcode/leetcode/majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        n = len(nums)
+        return nums[n//2]
+        
+                 
+             
+            
+        
