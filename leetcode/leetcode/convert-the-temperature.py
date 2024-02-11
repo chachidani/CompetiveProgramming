78521c2dbfe580623a885757bbef77a5a2984a3f
chachidani <141312783+chachidani@users.noreply.github.com>
2024-02-11 04:21:32 +0300

    Add solution for Convert the Temperature

---
+++ leetcode/leetcode/convert-the-temperature.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        temp = []
+        kal = celsius +273.15
+        far = (celsius*1.80) +32.00
+        temp.append(kal)
+        temp.append(far)
+        return temp
