07872fc569c03eb3be516ff7eee4c47471b11e8c
chachidani <141312783+chachidani@users.noreply.github.com>
2024-06-26 23:46:25 +0300

    Created 'C. Given Length and Sum of Digits...' by A2SV Hub

---
+++ 26-Jun-2024/C. Given Length and Sum of Digits... 173111.py
@@ -0,0 +1,31 @@
+# Problem: C. Given Length and Sum of Digits... - https://codeforces.com/contest/489/problem/C
+
+m, s = list(map(int, input().split()))
+total = s
+
+maxi = []
+
+for i in range(m):
+    curr = 9 if s >= 9 else s
+    s -= curr
+
+    maxi.append(curr)
+
+# print(maxi)
+
+s = total
+
+curr = 1 if s < 9 * (m - 1) else s - ((m - 1) * 9)
+s -= curr
+
+mini = [curr]
+for i in range(1, m):
+    curr = 0 if s < 9 * (m - i - 1) else s - ((m - i - 1) * 9)
+    s -= curr
+    mini.append(curr)
+
+left = sum(mini)
+right = sum(maxi)
+
+
+print(*["".join(map(str, mini)), "".join(map(str, maxi))] if left == right == total else [-1, -1])
