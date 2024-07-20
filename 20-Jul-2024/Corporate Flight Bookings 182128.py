f8f30c16b48d80ca305936b6e699f1eb240044c3
chachidani <141312783+chachidani@users.noreply.github.com>
2024-07-20 15:37:38 +0300

    Created 'Corporate Flight Bookings' by A2SV Hub

---
+++ 20-Jul-2024/Corporate Flight Bookings 182128.py
@@ -0,0 +1,24 @@
+# Problem: Corporate Flight Bookings - https://leetcode.com/problems/corporate-flight-bookings/
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        curr = [0]*n
+        pref = []
+        a,b,k = 0,1,2
+        for i in range(len(bookings)):
+            if bookings[i][b] < n:
+                curr[bookings[i][a]-1] += bookings[i][k]
+                curr[bookings[i][b]] += -bookings[i][k]
+            else:
+                curr[bookings[i][a]-1] += bookings[i][k]
+        
+        summ = 0
+        for num in curr:
+            summ +=num
+            pref.append(summ)
+        return pref
+                
+
+
+
+        
