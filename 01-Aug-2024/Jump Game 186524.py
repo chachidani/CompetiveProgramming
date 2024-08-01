28e2156543c35f7550fcd4d0f10589d40c61264d
chachidani <141312783+chachidani@users.noreply.github.com>
2024-08-01 16:41:45 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 01-Aug-2024/Jump Game 186524.py
@@ -0,0 +1,20 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if 0 not in nums:
+            return True
+        ans = True
+        position = len(nums) - 1
+        for i in range(len(nums)-2,-1,-1):
+            if position - i > nums[i]:
+                ans = False
+            else:
+                position -= position - i
+                ans = True
+        return ans
+
+
+
+
+        
