30d7098bc3af47e49a99112f72a6b250e9bd2880
Nardos Daniel <141312783+chachidani@users.noreply.github.com>
2024-08-12 00:24:05 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 11-Aug-2024/Group Anagrams 190808.py
@@ -0,0 +1,11 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
