d8de001953691c85924a50b6ab706b3c29688f56
Nardos Daniel <141312783+chachidani@users.noreply.github.com>
2024-10-21 18:44:44 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 21-Oct-2024/Unique Paths 221123.py
@@ -0,0 +1,8 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        return math.factorial(m+n-2)//(math.factorial(m-1)*math.factorial(n-1))
+        
+
